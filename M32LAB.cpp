2913fd21cfa2872d83f97ee5688b375d4e57fafc
kevinanelson357 <kevin.a.nelson357@gmail.com>
2026-03-12 18:23:09 +0000

    Huzzah it works

--- M32LAB.cpp
+++ M32LAB.cpp
@@ -11,3 +11,20 @@ using namespace std;
 int main() {
     double percentage;
     char letterGrade;
+    cout << "Please enter you percentage grade: ";
+    cin >> percentage;
+// We've got to define what percentage equals what grade
+    if (percentage >= 90) { 
+        letterGrade = 'A';}
+    else if (percentage >= 80) {
+        letterGrade = 'B';}
+    else if (percentage >= 70) {
+        letterGrade = 'C';} 
+    else if (percentage >= 60) {
+        letterGrade = 'D';}
+    else if (percentage <=59) {
+        letterGrade = 'F';}
+cout << "You have a " << letterGrade << " grade." << endl;
+return 0;
+    }
+    
