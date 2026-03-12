5411832509d003bf0d119b35293865e001f41a67
kevinanelson357 <kevin.a.nelson357@gmail.com>
2026-03-12 17:08:58 +0000

    Finally started my 2nd lab

---
+++ M32LAB.cpp
@@ -0,0 +1,13 @@
+/*CSC-134
+Kevin Nelson
+M3T2
+3-12-26 */
+
+// This is going to convert percentage grades into letter grades
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    double percentage;
+    char letterGrade;
