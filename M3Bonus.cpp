f502cbab2e0d9d5d8c9bb2cf26e23ba953b483c6
kevinanelson357 <kevin.a.nelson357@gmail.com>
2026-03-13 02:30:32 +0000

    Finally got the bugs out had to put return in after mad wizard

--- M3Bonus.cpp
+++ M3Bonus.cpp
@@ -15,14 +15,14 @@ int main() {
     int choice;
     cout << "You wake up in a dark room with a terrible headache. Your weapons have been taken.\n";
     cout << "You must eascape before the Mad Wizard has you tortured.\n";
-    cout << " You see a door to your left, a window to the front, and a staircas to your right.\n";
+    cout << " You see a door to your left, a window to the front, and a staircase to your right.\n";
     cout << "Type 1 foor the door, 2 for the window, and 3 for the stairs:";
     cin >> choice;
     if (choice == 1) {
         cout << " The door opens revealing a guard. He strikes you and slams the door.\n";
     }
         else if (choice == 2) {
-            cout << " You leap out recklessly. You legs snap as you hit the courtyard. \n";
+            cout << " You leap out recklessly. Your legs snap as you hit the courtyard. \n";
             cout << " You will now be tortured be the Mad Wizard.";
         }
         else if (choice == 3) {
@@ -32,15 +32,20 @@ int main() {
             if (choice == 1) {
                 cout << "The door opens revealing the keep's courtyard. You can run for it or sneak out.\n";
                 cout << "Type 1 to Run or 2 to Sneak.";
+            }
+            else if (choice == 2) {
+                cout << "You've found the Mad Wizard's study. He grins at you as you feel you're body freeze\n";
+                cout << " You will now be tortured by the Mad Wizard.";
+                return 0;
+            }
+
+            }
                 cin >> choice;
                 if (choice ==1) {
-                    cout << "You run for it. The guard give chas but you are faster. You escape into the forest.";
+                    cout << "You run for it. The guard give chase but you are faster. You escape into the forest.";
+                }
                     else if (choice == 2) {
                         cout << "You hear a laugh behind you. The guard stabs you with his spear. You are dead.";
                     }
                     
                 }
-            }
-        }
-    }
-}
