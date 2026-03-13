c1143ef4838ee0de545f3ba5e0bfd799c9a65b73
kevinanelson357 <kevin.a.nelson357@gmail.com>
2026-03-13 03:02:27 +0000

    It is not printing a number that I expected, I'll try again tomorrow.

---
+++ M4HW.cpp
@@ -0,0 +1,19 @@
+/* CSC-134
+Kevin Nelson
+M4HW
+3-12-26*/
+
+// We're making a multiplication table for bronze first which will be multiples of 5
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main() {
+    cout << "We're going to multiply by 5s today.\n";
+    cout << "Enter a number between 1 and 10: ";
+    int number;
+    double result;
+    result = number * 5;
+    cin >> number;
+    cout << "Times 5 equals " << result << endl;
+    return 0;
+}
