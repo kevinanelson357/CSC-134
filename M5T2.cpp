022b7827c7500d514ddd7c743ff5263b4d962a36
kevinanelson357 <kevin.a.nelson357@gmail.com>
2026-04-24 18:48:05 +0000

    Finishing the squares

--- M5T2.cpp
+++ M5T2.cpp
@@ -4,12 +4,16 @@ so 5 would become 25, etc. */
 #include <string>
 using namespace std;
 
-// declare a function
-int square(int number);
+// declare a function where a user can input a number and it will be squared and then printed out
+void squareNumber() {
+    cout << "Enter a number to be squared: ";
+    double number;
+    cin >> number;
+    double result = number * number;
+    cout << "The square of " << number << " is " << result << endl;
+}
+// main function to call the squareNumber function
 int main() {
-    int number = 5;
-    int answer;
-    answer = square(number);
-    cout << "The answer is " << answer << endl;
+    squareNumber();
     return 0;
 }
