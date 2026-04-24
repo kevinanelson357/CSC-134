ced6b026749920f46a0fe8acefb469986cc89fe9
kevinanelson357 <kevin.a.nelson357@gmail.com>
2026-04-24 17:34:12 +0000

    Set up void function need to run

---
+++ M5T1.cpp
@@ -0,0 +1,13 @@
+/* For the purposes of this one we're making void functions */
+#include <iostream>
+using namespace std;
+
+// declare a function
+void printAnswer(int answer);
+
+int main() {
+    int answer = 7;
+    printAnswer(answer);
+    return 0;
+
+}
